5928166d660b52fcb8c35593f1bb7d369d84de79
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-01-24 23:11:13 +0300

    div

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -5,8 +5,7 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 class App extends React.Component {
     render() {
-        return ( <
-            div >
+        return ( < div className = { styles.container } >
             <
             Cards / >
             <
